4aa506b1028bcd2ae3ae82ea554a248dcde73301
cxie0001trevor <trevor8961@gmail.com>
2018-07-31 00:32:10 +1000

    Finish basic JavaScript learning.

--- JavaScript_practice/Basic.js
+++ JavaScript_practice/Basic.js
@@ -21,4 +21,71 @@
 // console.log("message"): print in console
 // prompt(): pop up a dialog with an input box for user to enter something (string)
 //          you can assign the return value to a variable and save it for use.
-//
+
+// 5. Comparison Operators
+// Comparison: ==, !==, <, >, <=, >=,
+// ** Unlike another language, JavaScript will treats numeric string have the
+//    same equality as numeric value, for example:
+//        "2" == 2 --> true
+//    > This is known as type coercion: JavaScript try to convert both object
+//      to similar data types then can perform the comparison.
+//    However if using === triple-equal, it can be use to compare both value and
+//    and data types:
+//        "2" === 2 --> false
+//        "5" === "2" --> false
+//    About inequality, use !== to check inequality not just the value but also
+//    data type. for example:
+//        5 !== "5" --> true
+//    if only check the inequality about the value:
+//        5 != "5" --? false
+// ** Special case: It is vary common for programming language for 0 and 1 to be
+//    substitutes for false and true:
+//        true == 1 --> true
+//        false == 0 --> true
+//    As mentioned before, if compare by using triple-equal sign, both value and
+//    type will be performed to check:
+//        true === 1 --> false
+// ** More strange behaviour: null and undefined:
+//    null and undefined can be euqal when perform type coercion:
+//        null == undefined  --> true
+//    However, as mentioned before, they are two different type:
+//        null === undefined --> false
+//    Also, another situation:
+//        NaN == NaN --> false
+//    > Need care with that
+
+// 6. Logic operatiors
+// * AND: &&
+// * OR: ||
+// * opposite: !
+
+// 7. Control flow
+// 1) if statement:
+//   if (condition){
+//     //excute some codes;
+//   } else if(condition){
+//     //execute other codes;
+//   }
+// 2) while loop:
+//   while(conditon){
+//     //excute some codes
+//   }
+//   ** while can be break by using if statement to check inner condition. When
+//      certain condition matches, break come to perform and while loop stop.
+// 3) for loop:
+//   for (statement1; statement2; statement3){
+//      //Execute some codes;
+//   }
+//   ** Conditions checking order:
+//      • statement1: executed before the loop start.
+//      • statement2: define the condtion for running the loop.
+//      • statement3: execute each time after the loop cycles through
+//   Example:
+//    var word = "alphabetic";
+//    for (var i = 0; i<word.length; i++){
+//       console.log("Letter: " + word[i]);
+//    }
+//   ** if only 1 statement to be executed, then it can be simplified as following:
+//      for (var i = 0; i<word.length; i++) console.log("Letter: " + word[i]);
+
+// 8. 
---
+++ JavaScript_practice/new/myscript.js
@@ -0,0 +1,22 @@
+var depos = prompt("How much would you like to deposit?");
+var limit = 1000;
+// var retry = 0;
+// while (depos>limit){
+//   if (retry===2){
+//     alert("Excess maximum try!")
+//     depos = 0;
+//     break;
+//   }
+//   alert("Deposite should not large than the limit: 1000.")
+//   var depos = prompt("Please enter your deposit?");
+//   retry++;
+// }
+// alert("You have successful deposite "+ depos +" dollar.")
+
+
+//for loop
+
+
+
+
+console.log("Successful!");
