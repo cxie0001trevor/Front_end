7c6392e268aa7a6f20ea4296d69ff6f8b2864408
cxie0001trevor <trevor8961@gmail.com>
2018-07-30 19:32:55 +1000

    update

---
+++ JavaScript_practice/Basic.js
@@ -0,0 +1,24 @@
+// 1. Basic types
+// interger and floatting point number are all treated as number in JavaScript
+// boolean value are in lowercase: true/false.
+// undefined and null are both basic data type but not the same same thing.
+// basic arithmatical opertation in number: +,-,/,*,**,%,
+
+// 2. String
+// String concatenation: "JavaScript" + " is super cool" = "JavaScript is super cool"
+// String length: "hello".length
+// Special escape characters: "/n", "/t",
+// Treat reserved characters as string: "\"quote\"" --> ""quote""
+// Indexing String (0-based): "hello"[1] --> "e"
+
+// 3. Variable
+// Assignment: var varName = varValue; //need semi-column at the end, statment need it.
+// undefined means you define sth but never assign value for it: var name;
+// if null value was assigned to a var, it equals to define nothing. that var won't exist either
+
+// 4. Output
+// alert("message"): pop up dialog with message
+// console.log("message"): print in console
+// prompt(): pop up a dialog with an input box for user to enter something (string)
+//          you can assign the return value to a variable and save it for use.
+//
